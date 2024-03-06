6f8d696d064f233539f463c3ad632ee53f24a9e4
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-06 16:59:44 -0500

    link to about page from home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container mx-auto px-4 max-w-6xl">{children}</div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
+import paths from "@/app/paths";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <h1>Welcome to Badger</h1>
+      <h2>
+        <Link href={paths.about()}>About</Link>
+      </h2>
+    </div>
+  );
 }
